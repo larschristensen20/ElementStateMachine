f74669b1abd5090331f821d6b36a270ce9092e31
Lars <lars.christensen20@gmail.com>
2019-02-13 18:03:35 +0100

    Added Event class

---
+++ ElementStateMachine/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElementStateMachine
+{
+    /// <summary>
+    /// A generic event class, where an incoming event is represented as a string
+    /// This is a slightly modified version of 
+    /// Ulrik Pagh Schultz Event found at https://github.com/ulrikpaghschultz/MDSD.git && MDSD/src/statemachine/generic/Event.java
+    /// </summary>
+    public class Event
+    {
+        /// <summary>
+        /// The code of the event, as a string.
+        /// </summary>
+        private string code;
+
+        /// <summary>
+        /// Constructs a new Event with the given code
+        /// </summary>
+        /// <param name="code">the string representation of the event name</param>
+        public Event(string code) => this.code = code;
+
+        /// <summary>
+        /// Get a string representation of the event code.
+        /// </summary>
+        /// <returns>the event code</returns>
+        public string Code() => code;
+    }
+}
