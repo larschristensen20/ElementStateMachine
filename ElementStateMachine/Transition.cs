dcf96d62266e8a7698c0595a723cd7d4d1040585
Lars <lars.christensen20@gmail.com>
2019-02-13 18:03:16 +0100

    Added a transition class

---
+++ ElementStateMachine/Transition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ElementStateMachine
+{
+    public class Transition<T>
+    {
+        private string targetState;
+
+        public Transition(string targetState) { this.targetState = targetState; }
+
+        public string Action(T extendedState)
+        {
+            Effect(extendedState);
+            return targetState;
+        }
+
+        public string GetTarget() => targetState;
+
+        protected void Effect(T extendedState) {; }
+
+        public bool IsApplicable(T extendedState) => true;
+       
+    }
+}
