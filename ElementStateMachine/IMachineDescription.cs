51e6c62a48926af6a9675116e7328a7445eba6d2
Lars <lars.christensen20@gmail.com>
2019-02-13 18:03:46 +0100

    Added machine description interface

---
+++ ElementStateMachine/IMachineDescription.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElementStateMachine
+{
+    public interface IMachineDescription<T> where T: AbstractRuntimeState<T>
+    {
+        List<State<T>> GetAllStates();
+
+        T CreateRuntimeState();
+    }
+}
