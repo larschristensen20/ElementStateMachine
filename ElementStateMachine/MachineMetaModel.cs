4e9947e6a7d29e558306568d7642fdf8cba1564b
Lars <lars.christensen20@gmail.com>
2019-02-14 19:52:43 +0100

    Implemented a machine meta model

--- ElementStateMachine/MachineMetaModel.cs
+++ ElementStateMachine/MachineMetaModel.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ElementStateMachine
 {
-    class MachineMetaModel
+    public class MachineMetaModel : IMachineDescription<GenericRuntimeState>
     {
+        private List<State<GenericRuntimeState>> allStates;
+
+        private HashSet<string> extendedStateVariables;
+
+        public MachineMetaModel(List<State<GenericRuntimeState>> states, HashSet<string> variables)
+        {
+            this.allStates = new List<State<GenericRuntimeState>>(states);
+            this.extendedStateVariables = new HashSet<string>(variables);
+        }
+
+        public GenericRuntimeState CreateRuntimeState()
+        {
+            return new GenericRuntimeState(extendedStateVariables);
+        }
+
+        public List<State<GenericRuntimeState>> GetAllStates()
+        {
+            return allStates;
+        }
+        
+        public HashSet<string> GetExtendedStateVariables()
+        {
+            return extendedStateVariables;
+        }
+
     }
 }
+
+
